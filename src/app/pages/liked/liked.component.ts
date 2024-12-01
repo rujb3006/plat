3101f52fbb431b8b2c29768cbce2e522ed37ba1c
rujb3006 <ruthjbrandao97@outlook.com>
2024-11-30 21:56:11 -0300

    Primeiro commit - Adicionando o projeto da plataforma de vídeos

---
+++ src/app/pages/liked/liked.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-liked',
+  imports: [],
+  templateUrl: './liked.component.html',
+  styleUrl: './liked.component.css'
+})
+export class LikedComponent {
+
+}
